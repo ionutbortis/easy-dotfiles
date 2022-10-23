a53555ee531e3ac6ee1617e72e434c90c1bfd2dd
ionutbortis <ionutbortis@gmail.com>
2022-10-23 17:06:07 +0300

    Added guide comment for SUPPORTED_DISTROS config

--- sample/scripts/defaults.sh
+++ sample/scripts/defaults.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# The 'SUPPORTED_DISTROS' configuration should be left as a bash array 
+# and its elements should be STRICTLY only letters and numbers.
+# No spaces, dashes or other characters are allowed!
 SUPPORTED_DISTROS=( "fedora" "ubuntu" )
 
 DEFAULT_HOST_NAME="myPC"
