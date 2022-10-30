236705621b372c1698b3f2991602467194dc103e
ionutbortis <ionutbortis@gmail.com>
2022-10-30 12:00:36 +0200

    Very explanatory comment for SUPPORTED_DISTROS

--- sample/scripts/defaults.sh
+++ sample/scripts/defaults.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
 
-# The 'SUPPORTED_DISTROS' configuration should be left as a bash array 
-# and its elements should be STRICTLY only letters and numbers.
-# No spaces, dashes or other characters are allowed!
+# The 'SUPPORTED_DISTROS' configuration should be left as a bash array and its elements 
+# should be strings containing STRICTLY only letters, numbers and the underscore _ character.
+#
+# No spaces, dashes or other characters are allowed inside distro names except underscore!
+#
+# If you want to use multiple words, you can use the camel case style or an underscore:
+# => camelCase: "popOS" "linuxMint" "openSUSE"
+# => underscore: "pop_OS" "linux_mint" "open_SUSE"
+# 
+# The corresponding distro folder names inside the [ <dotfiles>/private/scripts ] folder
+# must match EXACTLY the SUPPORTED_DISTROS entries. 
+#
+# When running the [ <dotfiles>/scripts/install.sh ] script you are prompted to select the
+# distro for setup and it will try to find a custom distro setup file to invoke:
+# [ <dotfiles>/private/scripts/SUPPORTED_DISTRO_NAME/setup.sh ]
+#
+# Also, the install script will gather the list of apps to be installed by using the selected
+# distro for setup. See the [ <dotfiles>/private/config/apps/config.json ] file.
+#
 SUPPORTED_DISTROS=( "fedora" "ubuntu" )
 
 DEFAULT_HOST_NAME="myPC"
