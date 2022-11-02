3f5425d5d4ee56a48aff2d39f8a4c51dcc3e837a
ionutbortis <ionutbortis@gmail.com>
2022-11-02 11:51:37 +0200

    Added git reset script

---
+++ scripts/git/reset.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+sources() {
+  local script_folder="$( dirname "$(realpath -s "${BASH_SOURCE[0]}")" )"
+  
+  source "$script_folder/../common/vars.sh"
+  source "$script_folder/../common/utils.sh"
+
+}; sources
+
+setup_log_file "git-reset"
+
+reset() {
+  local folder="$1"
+
+  echo -e "\nResetting changes in [ $folder ]..."
+
+  cd "$folder"
+
+  git reset --hard @{u}
+  git clean -df
+  git pull
+}
+
+prompt_user "[ WARN ] This will reset all the unpushed changes from your local copy of <dotfiles> !"
+
+reset "$PRIVATE_FOLDER"
+reset "$PROJECT_ROOT"
