c6ae7eb7f201b61598c02f88aed2aff263e844a1
ionutbortis <ionutbortis@gmail.com>
2022-11-20 17:10:10 +0200

    Simpler gnome shell version extraction

--- scripts/common/vars.sh
+++ scripts/common/vars.sh
@@ -50,4 +50,4 @@ ANACRON_SCRIPT_PREFFIX="$USER-$PROJECT_NAME-"
 ANACRON_ACTIONS=( "export" "import" )
 
 # This method of determining the version only works for Gnome 40+
-GNOME_SHELL_VERSION="$(gnome-shell --version | cut --delimiter=' ' --fields=3 | cut --delimiter='.' --fields=1)"
+GNOME_SHELL_VERSION="$(gnome-shell --version | awk -F '[ .]' '{ print $3 }')"
