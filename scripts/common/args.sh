17b9efb3248b9da7ec083f82e775f161555da1e6
ionutbortis <ionutbortis@gmail.com>
2022-12-30 19:02:17 +0200

    Unset global vars so we don't clash with others

--- scripts/common/args.sh
+++ scripts/common/args.sh
@@ -15,13 +15,15 @@
 while [[ $# -gt 0 ]]; do
   [[ "$1" =~ ^[--] && "${1#--}" ]] || { shift; continue; }
 
-  read -ra argument_split <<< "${1/=/ }"
+  read -ra arg_split <<< "${1/=/ }"
 
-  variable_name="$(echo "${argument_split[0]#--}" | tr '-' '_')"
-  variable_value="${argument_split[1]:-"_"}"
+  var_name="$(echo "${arg_split[0]#--}" | tr '-' '_')"
+  var_value="${arg_split[1]:-"_"}"
 
   # Safe eval => http://mywiki.wooledge.org/BashFAQ/006#eval
-  eval "${variable_name^^}_ARG=\$variable_value"
+  eval "${var_name^^}_ARG=\$var_value"
 
   shift
 done
+
+unset arg_split var_name var_value
